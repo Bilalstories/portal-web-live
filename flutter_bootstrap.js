08a76bb0c14a0357be622c01a1a2d5129971cf72
Bilal@21 <bilalpatel5637@gmail.com>
2026-06-26 21:40:12 +0530

    🚀 Static pages + Read Guide buttons for SEO

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2450166246"
+    serviceWorkerVersion: "1720254844"
   }
 });
