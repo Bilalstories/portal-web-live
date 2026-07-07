45b74994d8c6462b2407be468e5435968b158787
Bilal@21 <bilalpatel5637@gmail.com>
2026-07-07 16:29:36 +0530

    ✅ Fixed static pages with categories and services

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -52,14 +52,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "1aadade1058a048b88bae69c64794ab6
 "favicon.png": "cf49ea431424f5afb3ac81b6ad7181b3",
 "firebase-messaging-sw.js": "aadaa9d55d7949175ca7c8d1ccb0968b",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "f5eb36b3683239bee1b662c21ff6f0ad",
+"flutter_bootstrap.js": "b926d289aaa445da28fdebf56ff53488",
 "icons/Icon-192.png": "cf49ea431424f5afb3ac81b6ad7181b3",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "dc25e1faf5060aab5f638bbb8fbeba11",
 "/": "dc25e1faf5060aab5f638bbb8fbeba11",
-"main.dart.js": "7f7123e4985b908dc968895d7378a2cc",
+"main.dart.js": "4b4222a08067b594a3a4d0f22aeae8a0",
 "manifest.json": "c6336d79b2b5e12fef3b104de85f67fc",
 "version.json": "57a61dd3aeda1eb80db7465b1e563ed0"};
 // The application shell files that are downloaded before a service worker can
