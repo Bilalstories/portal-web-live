b0a40f5fbc3cb5cad2a2f5ea4afab6429158829d
Bilal@21 <bilalpatel5637@gmail.com>
2026-08-26 16:07:09 +0530

    ✅ Final navbar fix: search, language, theme, login all working

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3361215451"
+    serviceWorkerVersion: "155194447"
   }
 });
